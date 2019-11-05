994ef3cbae1fd33a8be3ea1f2cf4217d30593148
giannisgioule <giannisgioule@gmail.com>
2019-11-05 16:13:26 +0000

    Added upload csv error. Changed the description of the max features parameters in the HTML files

--- apply_model/views.py
+++ apply_model/views.py
@@ -8,6 +8,8 @@ from .backend import BackendRegression
 from django.core.files import File
 from .post_options import PostOptions
 
+from django.contrib import messages
+
 
 # Create your views here.
 def home(request):        
@@ -19,75 +21,80 @@ def regression(request):
 def upload(request):    
 
     if request.method=='POST':        
-        uploaded_file=request.FILES['regression_dataset']                     
+        uploaded_file=request.FILES['regression_dataset'] if 'regression_dataset' in request.FILES else False  
         
-        uploaded_filename=uploaded_file.name
-        uploaded_filename=uploaded_filename.split(".csv")[0]                
+        if uploaded_file == False:
+            messages.warning(request,'Please upload a .csv file first')                  
+            return render(request,'apply_model/regression.html')
+        else:
         
-        method=request.POST.get('regression_method')
-        approach=request.POST.get('regression_approach')
-        
-        post_request=PostOptions(approach)
+            uploaded_filename=uploaded_file.name
+            uploaded_filename=uploaded_filename.split(".csv")[0]               
+            
+            method=request.POST.get('regression_method')
+            approach=request.POST.get('regression_approach')
+            
+            post_request=PostOptions(approach)
 
-        if method=="linear_regression":            
-            hyperparameters=post_request.linear_regression(request)
-                         
-        elif method=="decision_tree":                        
-            hyperparameters=post_request.decision_tree(request)            
-                                                      
-        elif method=="mlp":                        
-            hyperparameters=post_request.mlp(request)            
+            if method=="linear_regression":            
+                hyperparameters=post_request.linear_regression(request)
+                            
+            elif method=="decision_tree":                        
+                hyperparameters=post_request.decision_tree(request)            
+                                                        
+            elif method=="mlp":                        
+                hyperparameters=post_request.mlp(request)            
 
-        elif method=="random_forest":                    
-            hyperparameters=post_request.random_forest(request)
-                
-        elif method=="svr":                        
-            hyperparameters=post_request.svr(request)
-        
-        approach_parameters=post_request.regression_approach(request)
+            elif method=="random_forest":                    
+                hyperparameters=post_request.random_forest(request)
+                    
+            elif method=="svr":                        
+                hyperparameters=post_request.svr(request)
+            
+            approach_parameters=post_request.regression_approach(request)
 
-        my_context={
-            "method":request.POST.get('regression_method'),
-            "approach":request.POST.get('regression_approach'),
-            "dataset":uploaded_file,   
-            "hyperparameters":hyperparameters,
-            "approach_parameters":approach_parameters
-        }                    
-        if approach == "grid_search":
-            my_context.update({"approach_parameters_kfold":{
-                "n_splits":int(request.POST.get("kfold_n_splits")),
-                "random_state":int(request.POST.get("kfold_seed")),
-                "shuffle":True
-            }
-            })
-        
-        model=BackendRegression(my_context)
-        model.backend()
-        model.path="static/apply_model/"+uploaded_filename+'_'+method+'_'+approach+".sav"
-        
-        global correct_filename,correct_file
+            my_context={
+                "method":request.POST.get('regression_method'),
+                "approach":request.POST.get('regression_approach'),
+                "dataset":uploaded_file,   
+                "hyperparameters":hyperparameters,
+                "approach_parameters":approach_parameters
+            }                    
+            if approach == "grid_search":
+                my_context.update({"approach_parameters_kfold":{
+                    "n_splits":int(request.POST.get("kfold_n_splits")),
+                    "random_state":int(request.POST.get("kfold_seed")),
+                    "shuffle":True
+                }
+                })
+            
+            model=BackendRegression(my_context)
+            model.backend()
+            model.path="static/apply_model/"+uploaded_filename+'_'+method+'_'+approach+".sav"
+            
+            global correct_filename,correct_file
 
-        correct_filename = uploaded_filename+'_'+method+'_'+approach        
-        correct_file = model.path
-        
-        # if os.path.exists("apply_model/static/apply_model/model.sav"):
-        #     os.remove("apply_model/static/apply_model/model.sav")
-        joblib.dump(model.model,"apply_model/"+model.path)        
-        
-        my_context.update({"model":model.model})        
-        my_context.update({"model_path":model.path})
-        my_context.update({"mae_train":model.metrics_training["mae"]})        
-        my_context.update({"mse_train":model.metrics_training["mse"]})        
-        my_context.update({"ex_variance_train":model.metrics_training["ex_variance"]})        
-        my_context.update({"r2_train":model.metrics_training["r2"]})        
+            correct_filename = uploaded_filename+'_'+method+'_'+approach        
+            correct_file = model.path
+            
+            # if os.path.exists("apply_model/static/apply_model/model.sav"):
+            #     os.remove("apply_model/static/apply_model/model.sav")
+            joblib.dump(model.model,"apply_model/"+model.path)        
+            
+            my_context.update({"model":model.model})        
+            my_context.update({"model_path":model.path})
+            my_context.update({"mae_train":model.metrics_training["mae"]})        
+            my_context.update({"mse_train":model.metrics_training["mse"]})        
+            my_context.update({"ex_variance_train":model.metrics_training["ex_variance"]})        
+            my_context.update({"r2_train":model.metrics_training["r2"]})        
 
-        if approach!="full_training":            
-            my_context.update({"mae_test":model.metrics_testing["mae"]})        
-            my_context.update({"mse_test":model.metrics_testing["mse"]})        
-            my_context.update({"ex_variance_test":model.metrics_testing["ex_variance"]})        
-            my_context.update({"r2_test":model.metrics_testing["r2"]})                                                  
+            if approach!="full_training":            
+                my_context.update({"mae_test":model.metrics_testing["mae"]})        
+                my_context.update({"mse_test":model.metrics_testing["mse"]})        
+                my_context.update({"ex_variance_test":model.metrics_testing["ex_variance"]})        
+                my_context.update({"r2_test":model.metrics_testing["r2"]})                                                  
 
-    return render(request,'apply_model/upload.html',my_context)
+            return render(request,'apply_model/upload.html',my_context)
 
 def predict(request):            
     return render(request,'apply_model/predict.html')
